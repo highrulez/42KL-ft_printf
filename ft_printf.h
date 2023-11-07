0ffd9dd37a7ff30d242b51480c9eea8f298dc6c3
Neal Caffrey <42662415+highrulez@users.noreply.github.com>
2023-11-08 05:59:23 +0800

    Update ft_printf.h
    update define linux and macOS for PTRNULL

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: aawgku-o <aawgku-o@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 15:55:23 by aawgku-o          #+#    #+#             */
-/*   Updated: 2023/11/07 02:45:37 by aawgku-o         ###   ########.fr       */
+/*   Updated: 2023/11/08 05:45:03 by aawgku-o         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,22 @@
 # include <stdarg.h> // va_list, va_start, va_arg, va_end
 # include <unistd.h> // write
 # include <stdlib.h> // malloc, free
-// # include <limits.h> // INT_MIN, INT_MAX, UINT_MAX
-// # include <stdint.h> // uintmax_t
-// # include <stddef.h> // size_t
-// # include <wchar.h> // wchar_t
+# include <stdint.h> // uintmax_t
 
-int	ft_putchar(char c);
-int	ft_printf(const char *word, ...);
-int	ft_print_c(char c);
-int	ft_print_d(int n);
-int	ft_print_p(unsigned long value, int asc);
-int	ft_print_s(char *s);
-int	ft_print_u(unsigned int nb);
-int	ft_print_x(unsigned int value, int asc);
-
+# if defined (__linux__)
+#  define PTRNULL "(nil)"
+# elif defined (__APPLE__)
+#  define PTRNULL "0x0"
 # endif
+
+int	ft_printpercent(void);
+int	ft_printf(const char *str, ...);
+int	ft_putchar(char c);
+int	percent(void);
+int	ft_print_s(char *s);
+int	ft_print_p(unsigned long long ptr);
+int	ft_print_d(int n);
+int	ft_print_u(unsigned int n);
+int	ft_print_x(unsigned int num, const char format);
+
+#endif
